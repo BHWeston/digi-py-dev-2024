3d80211384d1cd0d90af55c447b40b9ff92b7428
BHWeston <ben.hobbs@weston.ac.uk>
2024-11-01 14:42:41 +0000

    Introduction to using arrays in Python

---
+++ Python Fundamentals/07 - Using Arrays.py
@@ -0,0 +1,16 @@
+# Create a list:
+lNames = ["Ben", "Fred", "George"]
+
+# Output the list on the screen:
+print(lNames)
+
+# List are given a location, these start at 0
+print(lNames[0])
+
+# This adds an item onto the end:
+lNames.append("Billy")
+
+print(lNames)
+
+lNames.insert(1, "Frank")
+print(lNames)
