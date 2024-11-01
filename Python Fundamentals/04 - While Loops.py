e4a36565afcddfc0de34588177bf48e0cf6087bb
BHWeston <ben.hobbs@weston.ac.uk>
2024-11-01 15:02:43 +0000

    Example of using while loops within code

---
+++ Python Fundamentals/04 - While Loops.py
@@ -0,0 +1,9 @@
+# This program will keep running until the phrase "stop" is typed in by the user:
+
+# Declare the variable as having nothing inside
+sPrinterText = ""
+
+# Loop if not equal to "stop"
+while sPrinterText != "stop":
+    sPrinterText = input("Enter in text to display onto the screen, type stop to end the program: ")
+    print(sPrinterText)
