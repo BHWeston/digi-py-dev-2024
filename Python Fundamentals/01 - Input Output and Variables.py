ec6ff7c5a991f629f5e691573bac7fa9c79fd2f5
BHWeston <ben.hobbs@weston.ac.uk>
2024-11-01 14:46:31 +0000

    Creation of input, output and variables within Python

---
+++ Python Fundamentals/01 - Input Output and Variables.py
@@ -0,0 +1,16 @@
+# The below is used to output information to a screen:
+print("Hello World")
+
+# Like algebra, the below  is a way in which I can store information. We have made a location (known as a variable) called "item" and added a value within this:
+item = "banana"
+
+# This allows me to print what is stored above
+print(item)
+
+# Variables can be updated, code in this program works sequentially, so once the below line is ran, the "item" is changed from "banana" to "apple": 
+item = "apple"
+
+
+# The input() will be used to allow for a user to type something into the program
+item = input("Enter in an item you want to display: ")
+print(item)
