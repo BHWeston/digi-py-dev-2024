d6d2a3980a70af8b26f9c96afe8c0645efc6b007
BHWeston <ben.hobbs@weston.ac.uk>
2024-11-04 14:34:53 +0000

    Updated comments within linear search algorithm

--- Algorithms/08 - Linear Search.py
+++ Algorithms/08 - Linear Search.py
@@ -9,9 +9,7 @@ sNameToFind = input("Enter in the name that you want to find: ")
 # Create a "flag" that can be used within the algorithm:
 bNameFound = False
 
-# len() gets the length
-print(len(lPeopleList))
-
+# len() gets the length, this will be used so we can iterate through the list that many times:
 iNumberOfItems = len(lPeopleList)
 
 # We are going to check through each item within the list, to do this, we are going to use a FOR loop. FOR loops contain the following
@@ -20,7 +18,9 @@ for iCounter in range(iNumberOfItems):
     # Check if the name to find is the same as the item in the list:
     if sNameToFind == lPeopleList[iCounter]:
         print("You have found the name")
+        # This will look to confirm that the name has been found:
         bNameFound = True
 
+# If the name has not been found within the algoithm, the below will look to be displayed
 if bNameFound == False:
     print("You have not found the name")
