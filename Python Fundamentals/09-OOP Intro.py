f3e80009b882e1a301472d15217c94d529b3e328
BHWeston <ben.hobbs@weston.ac.uk>
2024-11-08 10:26:21 +0000

    Update to add introduction to OOP

---
+++ Python Fundamentals/09-OOP Intro.py
@@ -0,0 +1,16 @@
+# Introduction to how classes work within Python:
+
+# Think of classes as a way of creating a template within our work:
+class exampleClass:
+    x = 20
+
+# Once we create a class, we can use that class name to create "objects" within our work:
+objectName = exampleClass()
+# All objects that we create will have access to any variables/methods that we create within the class e.g. in this case the variable X:
+print(objectName.x)
+
+# TERMS TO INVESTIGATE AROUND OOP, look at what each of the below mean and how these link to coding (find examples):
+# Encapsulation
+# Inheritance
+# Polymorphism
+# Abstraction
