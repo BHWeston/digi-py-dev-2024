7861b1c62a8f70940bebeb7d71fb147820364a91
BHWeston <ben.hobbs@weston.ac.uk>
2024-11-11 14:46:43 +0000

    Uploading of SQL Intro and Matplotlib Testing

---
+++ Analysis/01-Matplot Test.py
@@ -0,0 +1,12 @@
+ # Import the library:
+import matplotlib.pyplot as plt
+
+# The data that we want to plot:
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 6, 8, 10]
+
+# Plot the values:
+plt.bar(x,y)
+
+# Show the plot:
+plt.show()
